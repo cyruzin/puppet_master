788191ace075c3e3408ef26c03acbdfb2e4bea5d
Cyro Dubeux <xorycx@gmail.com>
2021-03-22 18:30:22 -0300

    auth midleware draft comment

--- modules/shared/delivery/graphql/middleware/middleware.go
+++ modules/shared/delivery/graphql/middleware/middleware.go
@@ -31,5 +31,12 @@ func LoggerMiddleware(next http.Handler) http.Handler {
 // func AuthMiddleware(next http.Handler) http.Handler {
 // 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
+// 1 - Check JWT authenticity (Isolated function)
+// 2 - Extract user data (Isolated function)
+// 3 - Verify permissions (Isolated function)
+//  3.1 - Get all permissions from database or redis (Isolated function)
+//  3.2 - Check if the user permissions are valid and return true or false (Isolated function)
+// 4 - Return error or forward the request (Here)
+
 // 	})
 // }
